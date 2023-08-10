9259661e03717ffc18738744022858b2c5dfef50
Bruno Dias <brunodg.dev@gmail.com>
2023-08-10 17:24:10 -0300

    TopNav section updated

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -2,9 +2,9 @@
     <div id="TopNav" class="fixed bg-transparent z-30 flex items-center w-full h-[80px] shadow-sm">
         <div class="px-6 w-full h-[80px] flex items-center justify-between">
             <div class="flex items-center">
-                <img :src="beveldrive.logo" alt="Company logo" class="w-[60px] h-[60px]" />
+                <img :src="theCompany.logo" alt="Company logo" class="w-[60px] h-[60px]" />
                 <p class="mx-4 text-4xl">
-                    {{ beveldrive.name }}
+                    {{ theCompany.name }}
                 </p>
             </div>
             <div class="flex items-center mx-2">
@@ -26,7 +26,7 @@ interface Company {
     email: string;
 }
 
-const beveldrive: Company = {
+const theCompany: Company = {
     name: "Company",
     logo: "/images/logo_transparent.png",
     website: "https://company.com",
